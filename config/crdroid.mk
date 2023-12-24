296d4451ea75eaba5cba71c83cf142124df62e83
Notorious <35992771+TTTT55@users.noreply.github.com>
2023-12-24 19:25:36 +0530

    vendor: build GApps
    Switching to GApps only builds
    
    Signed-off-by: TTTT55 <iamtttt55@gmail.com>

--- config/crdroid.mk
+++ config/crdroid.mk
@@ -69,3 +69,6 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
+
+# GAPPS
+$(call inherit-product, vendor/gapps/common/common-vendor.mk)
